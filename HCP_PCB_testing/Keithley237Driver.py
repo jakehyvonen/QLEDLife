294e99b5accd948661a099961cff9bd4e04087a9
jakehyvonen <jakehyvonen@gmail.com>
2019-12-09 11:08:01 -0500

    began working on drivers to test Howland Charge Pump PCBs

---
+++ HCP_PCB_testing/Keithley237Driver.py
@@ -0,0 +1,47 @@
+import PrologixDriver as comm
+from time import sleep
+
+def SendMsg(message):
+    return comm.SendSerialCommandAndGetResponse(message)
+
+def SendMsgGetResponse():
+    var = input('enter command:')
+    serialResponse = SendMsg(var)
+    print('serialResponse:'+serialResponse)
+    #readResponse = comm.ReadGPIBMessage()
+    #print('readResponse:'+readResponse)
+
+def Initialize():
+    SendMsg('++addr 15')#set GPIB address to 15     
+    #SendMsg('F0,0X')#(THIS CAUSES HANGUP)set source: voltage, measure: current
+    SendMsg('B0.0,0,0X')#set bias 0V, autorange, no delay
+    SendMsg('G4,0,0X')#output data: measure value, ASCII, one line of data
+    SendMsg('P4X')#average 16 readings
+
+def TurnOutputOn():
+    SendMsg('N1X')
+
+def TurnOutputOff():
+    SendMsg('N0X')
+    SetVoltage(0.0)
+
+def SetVoltage(voltage):
+    val = str(voltage)
+    SendMsg('B'+val+',0,0X')
+
+def MeasureCurrent():
+    response = comm.ReadGPIBMessage()
+    return ParseCurrentString(response)
+
+def ParseCurrentString(s):
+    if('+'in s):
+        s=s.split('+',1)[-1]
+    if('I'in s):
+        s=s.split('I',1)[-1]
+    if(','in s):
+        s=s.split(',')[0]
+    return s
+    
+#Initialize()
+#while(True):
+#    SendMsgGetResponse()
