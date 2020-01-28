6eb97b3c128f0cace6829749cbcec1537743b292
nandreo33 <nathan.andreo33@gmail.com>
2020-01-28 15:37:12 -0500

    i2c error reporting

--- controller/DAC.cpp
+++ controller/DAC.cpp
@@ -6,13 +6,13 @@
 
 Dac::Dac(int maxCode, float maxCurrent, int numDevices) : m_maxCode(maxCode), m_maxCurrent(maxCurrent), m_numDevices(numDevices) {};
 
-static void i2cwrite(uint8_t i2cAddress, uint8_t command, uint16_t data)
+static byte i2cwrite(uint8_t i2cAddress, uint8_t command, uint16_t data)
 {
   Wire.beginTransmission(i2cAddress);
   Wire.write(command);
   Wire.write((uint8_t)(data >> 6));
   Wire.write((uint8_t)((data << 2) & 0xFF));
-  Wire.endTransmission();
+  byte status = Wire.endTransmission();
 }
 
 void Dac::begin()
@@ -45,9 +45,9 @@ int Dac::set_all_current(float f)
 
   //TODO - error check here
   uint8_t commandByte = AD5645R_CMD_WRITE_UPDATE_N | AD5645R_DAC_CHANNEL_ALL;
-  i2cwrite(AD5645R_BROADCAST_ADDR,commandByte,DacSetInt);
+  byte status = i2cwrite(AD5645R_BROADCAST_ADDR,commandByte,DacSetInt);
 
-  return 0;
+  return status;
 }
 
 int Dac::set_current(int device, int channel, float f)
@@ -66,11 +66,11 @@ int Dac::set_current(int device, int channel, float f)
 
   //TODO - error check here
   uint8_t commandByte = AD5645R_CMD_WRITE_UPDATE_N | channel;
-  i2cwrite(m_devices[device],commandByte,DacSetInt);
+  byte status = i2cwrite(m_devices[device],commandByte,DacSetInt);
 
   store_dac_int(device,channel,DacSetInt);
 
-  return 0;
+  return status;
 }
 
 /* ######################################################
