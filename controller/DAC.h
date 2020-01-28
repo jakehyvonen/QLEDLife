a20fce0df0d60cba2e384bf9c893703fba7ebcd7
nandreo33 <nathan.andreo33@gmail.com>
2020-01-28 15:29:30 -0500

    combined AD5645R drivers into DAC class completed basic functions of I2C DAC

--- controller/DAC.h
+++ controller/DAC.h
@@ -1,32 +1,41 @@
 #ifndef DAC_H
 #define DAC_H
 
-#include <SPI.H>
+#include <Wire.h>
+
+#define AD5645R_BROADCAST_ADDR   (0x8)
+
+#define AD5645R_DAC_CHANNEL_A    (0x0)
+#define AD5645R_DAC_CHANNEL_B    (0x1)
+#define AD5645R_DAC_CHANNEL_C    (0x2)
+#define AD5645R_DAC_CHANNEL_D    (0x3)
+#define AD5645R_DAC_CHANNEL_ALL  (0x7)
+
+#define AD5645R_CMD_WRITE_REG_N         (0x0 << 3)
+#define AD5645R_CMD_UPDATE_REG_N        (0x1 << 3)
+#define AD5645R_CMD_WRITE_UPDATE_ALL    (0x2 << 3)
+#define AD5645R_CMD_WRITE_UPDATE_N      (0x3 << 3)
+#define AD5645R_CMD_RESET               (0x5 << 3) 
 
 class Dac
 {
   public:
-    Dac(int maxCode, float maxCurrent, int numDev, int syncPin);
+    Dac(int maxCode, float maxCurrent, int numDevices);
     void begin();
-    void enable_SDO();
-    void set_all_current(float f);
+    int set_all_current(float f);
     int set_current(int device, int channel, float f);
-    float read_device_current(int device, int channel);
-    void test_write();
-    void test_enable_SDO();
-    void test_clr();
-    void test_read_register(byte reg, int ch, byte* buf);
 
   private:
-    const int MAX_DAC_CODE = 16383;
-    const float MAX_CURRENT = 10.0;
-    const int NUM_DEVICES = 1;
-    const int SYNC_PIN = 9;
-    SPISettings settingsA;
+    const int m_maxCode = ((1 << 14) - 1);   // 14-bits
+    const float m_maxCurrent = 10.0;
+    const int m_numDevices = 1;
+    const uint8_t m_devices[9] = {
+      0x10,0x12,0x13,
+      0x18,0x1A,0x1B,
+      0x1C,0x1E,0x1F
+    };
     int current_float_to_int(float f);
     float current_int_to_float(int i);
-    int transmit(byte *tx_buf, byte *rx_buf, int len);
-    void send_nop(byte* buf);
 };
 
 #endif
